7842b6b24630e4a635f93fcfc865e16f82582ef5
carlosMorenoQ <92289007+carlosMorenoQ@users.noreply.github.com>
2021-11-29 11:51:46 -0500

    parte4 del video: completando los casos de uso en el front

--- front/src/App.js
+++ front/src/App.js
@@ -4,22 +4,24 @@ import React, {
   useEffect,
   useRef,
   useState,
-  createContext
+  createContext,
 } from "react";
-// /************************* ↓ contexto ↓ ***************************************/
+/************************* ↓ contexto ↓ ***************************************/
 const HOST_API = "http://localhost:8080/api";
 const initialState = {
   list: [],
   item: {},
 };
 const Store = createContext(initialState);
-// /************************* ↑ contexto ↑ ***************************************/
-// /************************* ↓ componentForm ↓ **********************************/
+/************************* ↑ contexto ↑ ***************************************/
+/************************* ↓ componentForm ↓ **********************************/
 const Form = () => {
   const formRef = useRef(null);
-  const { dispatch, state: { item } } = useContext(Store);  
-  const [state, setState] = useState({item});
-
+  const {
+    dispatch,
+    state: { item },
+  } = useContext(Store);
+  const [state, setState] = useState(item);
 
   const onAdd = (event) => {
     event.preventDefault();
@@ -27,49 +29,87 @@ const Form = () => {
     const request = {
       name: state.name,
       id: null,
-      isCompleted: false
+      isCompleted: false,
     };
 
     fetch(HOST_API + "/todo", {
       method: "POST",
       body: JSON.stringify(request),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     })
-      .then(response => response.json())
+      .then((response) => response.json())
       .then((todo) => {
         dispatch({ type: "add-item", item: todo });
         setState({ name: "" });
         formRef.current.reset();
       });
-  }  
+  };
+
+  const onEdit = (event) => {
+    event.preventDefault();
+
+    const request = {
+      name: state.name,
+      id: item.id,
+      isCompleted: item.isCompleted,
+    };
+
+    fetch(HOST_API + "/todo", {
+      method: "PUT",
+      body: JSON.stringify(request),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((todo) => {
+        dispatch({ type: "update-item", item: todo });
+        setState({ name: "" });
+        formRef.current.reset();
+      });
+  };
 
   return (
     <form ref={formRef}>
       <input
         type="text"
         name="name"
+        defaultValue={item.name}
         onChange={(event) => {
           setState({ ...state, name: event.target.value });
         }}
       ></input>
-      <button onClick={onAdd}>Agregar</button>
+      {item.id && <button onClick={onEdit}>Actualizar</button>}
+      {!item.id && <button onClick={onAdd}>Agregar</button>}
     </form>
   );
 };
-// /************************* ↑ componentForm ↑ **********************************/
-// /************************* ↓ componentList ↓ **********************************/
+/************************* ↑ componentForm ↑ **********************************/
+/************************* ↓ componentList ↓ **********************************/
 const List = () => {
   const { dispatch, state } = useContext(Store);
 
   useEffect(() => {
     fetch(HOST_API + "/todos")
-      .then(response => response.json())
+      .then((response) => response.json())
       .then((list) => {
         dispatch({ type: "update-list", list });
       });
   }, [state.list.length, dispatch]);
+
+  const onDelete = (id) => {
+    fetch(HOST_API + "/" + id + "/todo", {
+      method: "DELETE",
+    }).then((list) => {
+      dispatch({ type: "delete-item", id });
+    });
+  };
+
+  const onEdit = (todo) => {
+    dispatch({ type: "edit-item", item: todo })
+  };  
 
   return (
     <div>
@@ -87,7 +127,13 @@ const List = () => {
               <tr key={todo.id}>
                 <td>{todo.id}</td>
                 <td>{todo.name}</td>
-                <td>{todo.isComplete}</td>
+                <td>{todo.isComplete === true ? "SI" : "NO"}</td>
+                <td>
+                  <button onClick={() => onDelete(todo.id)}>Eliminar</button>
+                </td>
+                <td>
+                  <button onClick={() => onEdit(todo)}>Editar</button>
+                </td>
               </tr>
             );
           })}
@@ -96,12 +142,28 @@ const List = () => {
     </div>
   );
 };
-// /************************* ↑ componentList ↑ **********************************/
-// /*************************** ↓ reducer ↓ *****************************************/
+/************************* ↑ componentList ↑ **********************************/
+/*************************** ↓ reducer ↓ *****************************************/
 function reducer(state, action) {
   switch (action.type) {
     case "update-item":
+      const listUpdateEdit = state.list.map((item) => {
+        if (item.id === action.item.id) {
+          return action.item;
+        }
+        return item;
+      });
+      return { ...state, list: listUpdateEdit, item: {} };
+
+    case "delete-item":
+      const listUpdate = state.list.filter((item) => {
+        return item.id !== action.id;
+      });
+      return { ...state, list: listUpdate };
+    case "update-list":
       return { ...state, list: action.list };
+    case "edit-item":
+      return { ...state, item: action.item };
     case "add-item":
       const newList = state.list;
       newList.push(action.item);
@@ -110,11 +172,13 @@ function reducer(state, action) {
       return state;
   }
 }
-// /*************************** ↑ reducer ↑ *****************************************/
-// /*************************** ↓ provider ↓ *****************************************/
+/*************************** ↑ reducer ↑ *****************************************/
+/*************************** ↓ provider ↓ *****************************************/
 const StoreProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  return <Store.Provider value={{ state, dispatch }}>{children}</Store.Provider>;
+  return (
+    <Store.Provider value={{ state, dispatch }}>{children}</Store.Provider>
+  );
 };
 // /*************************** ↑ provider ↑ *****************************************/
 
