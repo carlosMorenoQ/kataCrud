a49a4e3723c020329fb78f47ea8e4a335ec293ab
carlosMorenoQ <92289007+carlosMorenoQ@users.noreply.github.com>
2021-11-29 10:50:16 -0500

    parte3 del video: creacion del front, con verificacion de funcionamiento

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,129 @@
-import React from 'react';
+import React, {
+  useContext,
+  useReducer,
+  useEffect,
+  useRef,
+  useState,
+  createContext
+} from "react";
+// /************************* ↓ contexto ↓ ***************************************/
+const HOST_API = "http://localhost:8080/api";
+const initialState = {
+  list: [],
+  item: {},
+};
+const Store = createContext(initialState);
+// /************************* ↑ contexto ↑ ***************************************/
+// /************************* ↓ componentForm ↓ **********************************/
+const Form = () => {
+  const formRef = useRef(null);
+  const { dispatch, state: { item } } = useContext(Store);  
+  const [state, setState] = useState({item});
+
+
+  const onAdd = (event) => {
+    event.preventDefault();
+
+    const request = {
+      name: state.name,
+      id: null,
+      isCompleted: false
+    };
+
+    fetch(HOST_API + "/todo", {
+      method: "POST",
+      body: JSON.stringify(request),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then((todo) => {
+        dispatch({ type: "add-item", item: todo });
+        setState({ name: "" });
+        formRef.current.reset();
+      });
+  }  
+
+  return (
+    <form ref={formRef}>
+      <input
+        type="text"
+        name="name"
+        onChange={(event) => {
+          setState({ ...state, name: event.target.value });
+        }}
+      ></input>
+      <button onClick={onAdd}>Agregar</button>
+    </form>
+  );
+};
+// /************************* ↑ componentForm ↑ **********************************/
+// /************************* ↓ componentList ↓ **********************************/
+const List = () => {
+  const { dispatch, state } = useContext(Store);
+
+  useEffect(() => {
+    fetch(HOST_API + "/todos")
+      .then(response => response.json())
+      .then((list) => {
+        dispatch({ type: "update-list", list });
+      });
+  }, [state.list.length, dispatch]);
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>Nombre</td>
+            <td>¿Está completado?</td>
+          </tr>
+        </thead>
+        <tbody>
+          {state.list.map((todo) => {
+            return (
+              <tr key={todo.id}>
+                <td>{todo.id}</td>
+                <td>{todo.name}</td>
+                <td>{todo.isComplete}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+// /************************* ↑ componentList ↑ **********************************/
+// /*************************** ↓ reducer ↓ *****************************************/
+function reducer(state, action) {
+  switch (action.type) {
+    case "update-item":
+      return { ...state, list: action.list };
+    case "add-item":
+      const newList = state.list;
+      newList.push(action.item);
+      return { ...state, list: newList };
+    default:
+      return state;
+  }
+}
+// /*************************** ↑ reducer ↑ *****************************************/
+// /*************************** ↓ provider ↓ *****************************************/
+const StoreProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return <Store.Provider value={{ state, dispatch }}>{children}</Store.Provider>;
+};
+// /*************************** ↑ provider ↑ *****************************************/
 
 function App() {
   return (
-    <div>
-      front con react
-    </div>
+    <StoreProvider>
+      <Form />
+      <List />
+    </StoreProvider>
   );
 }
 
